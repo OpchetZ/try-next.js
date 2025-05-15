3499678b4811a94a372e20f907fb5f7884da9b0e
OpchetZ <varanyookasamsuk@gmail.com>
2025-05-15 20:16:42 +0700

    PATH SATATIC

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,11 +1,35 @@
-import { useRouter } from 'next/router'
+export async function getStaticPaths() {
+    const res = await fetch('https://dummyjson.com/products?limit=12');
+    const data = await res.json();
+    const paths = data.products.map((item) => {
+        return{
+            params: { id:String(item.id) },
+        }
+    
+})
+    return {
+        paths,
+        fallback: false,
+    }
+}
+export async function getStaticProps({params}) {
+    const id = params.id
+    const res = await fetch("https://dummyjson.com/products/" + id)
+    const data = await res.json()
+    console.log(data)
+    return {
+        props: {
+            products:data
+        }
+    }
+}
 
-export default function ProductDetail() {
-    const router = useRouter()
-    const { id } = router.query
+export default function ProductDetail({products}) {
+   
     return(
         <>
-        <h1>สินค้าชิ้นที่ : {id}</h1>
+        <h1>สินค้าชิ้นที่ : {products.id}</h1>
+        <h2>{products.title}</h2>
         </>
     )
 }
